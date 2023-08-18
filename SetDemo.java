0a86dda05d6b536a79e80a5b064caee4974a46d5
12-darshana <142590637+12-darshana@users.noreply.github.com>
2023-08-18 16:41:39 +0530

    Add files via upload

---
+++ SetDemo.java
@@ -0,0 +1,60 @@
+package com.collection;
+import java.util.Iterator;
+import java.util.TreeSet;
+public class SetDemo {
+
+	public static void main(String[] args) {
+
+		/*TreeSet ts=new TreeSet();
+		
+		ts.add(100);  ts.add(50);  ts.add(35);
+		ts.add(40);   ts.add(76);  ts.add(80);
+		
+		System.out.println("Data from Treeset:" +ts);
+		System.out.println("Printing by using iterator :");
+		
+		Iterator <Integer>it=ts.iterator();
+		
+		while(it.hasNext()) {
+			int no=(int)it.next();//Typecasting
+			if(no%2==0)
+			    System.out.println("Value is even");
+			else {
+				System.out.println("Value is odd");
+			}
+			
+		}
+		System.out.println("By using for each");
+		
+		for(Object object : ts) {
+			int no=(int) object;
+			if(no%2==0) {
+				System.out.println("Even");
+			}
+			else {
+			    System.out.println("Odd");
+			}
+		}
+		System.out.println("Total objects :"+ts.size());
+		System.out.println("Found? :" +ts.contains(40));
+		System.out.println("First value:"+ ts.first());
+		System.out.println("Greatest value"+ts.lower(80));
+		ts.remove(80);
+		System.out.println(ts);
+		System.out.println();
+	}
+    HashSet <Interger> hs=new HashSet<>();
+    
+    hs.add(155);   hs.add(150);  hs.add(180); 
+    hs.add(170);   hs.add(195);  hs.add(120; 
+    
+    System.out.println("Hashset data is:"+hs);
+    ts.adAll(hs);*/
+    
+    
+   /* LinkedHashSet <Integer> lhs=new LinkedHashSet<>();
+    
+    lhs.add(210);   lhs.add(205);  lhs.add(300); 
+    lhs.add(240);   lhs.add(230);  lhs.add(220); */
+    System.out.println();
+}
